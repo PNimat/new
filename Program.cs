64ba43f1fec3275d356b3e04a578145eaa97f890
nimat <niymet.p@ya.ru>
2022-11-22 23:46:19 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int i = -4;
+
+// for(int count = i; count<=-i; count++)
+// {
+//     Console.Write(count + " ");
+// }
+
+
+int f = Convert.ToInt32(Console.ReadLine());
+
+int c = -f;
+while(f>=c) 
+{
+    Console.Write(f+ " ");
+    f--;
+}
+
